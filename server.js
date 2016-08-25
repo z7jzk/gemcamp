34e8c4e30e05bc6815b04f0923f902b63fdfe4e2
kyburzjz <kyburzjz@gmail.com>
2016-08-25 18:03:16 +0000

    updated links and image for GEM Camp

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ io.on('connection', function (socket) {
   });
   
   socket.emit('message', {
-    text: 'Welcome to the chat application!'
+    text: 'Welcome to the official app of GEM Camp 2.0!'
   });
 });
 
