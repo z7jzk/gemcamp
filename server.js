e50d047225381786aa952f1b2141d50bc3b73d2b
kyburzjz <kyburzjz@gmail.com>
2016-08-28 00:28:36 +0000

    updated the bodyParser

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(__dirname + '/client'));
 app.set('views', __dirname + '/client');
 app.set('view engine', 'ejs');
 //app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var clientInfo = {};
 
